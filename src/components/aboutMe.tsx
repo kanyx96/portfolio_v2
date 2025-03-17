77f23cf76c36b8a8b9a0009d034b5a0375ed9e4f
kanyx96 <32454464+kanyx96@users.noreply.github.com>
2025-03-17 15:01:59 +0800

    Update aboutMe.tsx

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -2,7 +2,7 @@ import { Box, Avatar, Typography, List, ListItem, Grid2 } from "@mui/material";
 
 export function About_Me() {
   return (
-    <Grid2 container spacing={2} sx={{height: "100%", overflowY: "auto"}}>
+    <Grid2 container spacing={2} sx={{ height: "100%", overflowY: "auto" }}>
       <Grid2 size={8}>
         <Typography variant='h6'>{"About Me"}</Typography>
         <Box
@@ -28,12 +28,12 @@ export function About_Me() {
                   variant='subtitle2'
                   sx={{ fontSize: 16, textDecoration: "underline" }}
                 >
-                  Software Developer
+                  Engineer (Software)
                 </Typography>
               </ListItem>
               <ListItem>
                 <Typography variant='body1' sx={{ fontSize: 16 }}>
-                  Driven and Passionate
+                  Driven, Passionate and Relentless
                 </Typography>
               </ListItem>
               <ListItem>
@@ -43,6 +43,57 @@ export function About_Me() {
               </ListItem>
             </List>
           </Box>
+          <div>
+            <Typography variant='h6' sx={{ textDecoration: "underline" }}>
+              My Dream
+            </Typography>
+            <Typography variant='body1'>
+              I wish to work my whole life to build products (be it software or
+              hardware) that helps others - to solve real world problems.
+            </Typography>
+          </div>
+          <div>
+            <Typography variant='h6' sx={{ textDecoration: "underline" }}>
+              Journey as an Engineer
+            </Typography>
+            <Typography variant='body1'>
+              Being the son of a mechanical engineer, loving Legos and problem
+              solving, I always knew I wanted to be an engineer. As such, it was
+              without question that I would study engineering in University.
+              <br />
+              <br />
+              My first exposure to software engineering was in University. I was
+              studying Mechanical Engineering with direct Masters. I fell in
+              love with software becuase of how accessible it is. I found a
+              study of engineering where I can build what my imagination holds
+              without substantial upfront capital/resourses.
+              <br />
+              <br />
+              Every holiday, I had to come back to Singapore to fulfill my bond
+              with The Army and it was there that I demonstrated my capabilities
+              in software development. I was also lucky that when I come back to
+              fulfill my bond with the Army, it was the start of the pandemic
+              and personnel are needed to work in the COVID Technology Task
+              Force as software developers. I developed my skills from there and
+              took it further when I left the Army to work on my own personal
+              projects.
+            </Typography>
+          </div>
+          <div>
+            <Typography variant='h6' sx={{ textDecoration: "underline" }}>
+              Journey to Army
+            </Typography>
+            <Typography variant='body1'>
+              When I was 15, I was the chairperson of Nan Hua&apos;s Boys&apos;
+              Brigade Company. Being in an uniform group and being a history
+              buff (especially history on war), I have always dreamt about being
+              in the Army and I worked my ass off in Secondary School and JC to
+              get an Army Scholarship.
+              <br />
+              <br />I got into OCS and got a scholarship to study at University
+              College London (UCL).
+            </Typography>
+          </div>
         </Box>
       </Grid2>
       <Grid2 size={4}>
